2d231b979c47a5e781c3b336eea9b31b786e806a
DanyloSus <danil.sushko19@gmail.com>
2024-03-30 23:24:40 +0200

    Completed

--- app/signed/products/Product.tsx
+++ app/signed/products/Product.tsx
@@ -11,18 +11,20 @@ type ProductElementProps = {
 
 const ProductElement = (props: ProductElementProps) => {
   return (
-    <div className="bg-white border border-grey flex flex-col items-center">
+    <div className="bg-white border border-grey flex flex-col items-center sm:max-w-[444px] lg:max-w-[380px] rounded-lg">
       <Image
         src={`/${props.image}.png`}
         alt={props.name}
         width={380}
         height={187}
-        className="w-[380px] h-[187px] object-cover"
+        className="w-full h-[187px] object-cover"
       />
-      <div className="flex flex-col gap-3 py-8 items-center justify-center">
+      <div className="flex flex-col gap-0 py-8 items-center justify-center max-w-[239px] font-medium">
         <h3 className="text-[40px] text-black text-center">{props.name}</h3>
-        <p className="text-black text-center">${props.price}</p>
-        <p className="text-2l text-black w-[239px] text-center">{props.text}</p>
+        <span className="text-black text-center">${props.price}</span>
+        <span className="text-2l text-black w-[239px] text-center mb-4">
+          {props.text}
+        </span>
         <Button
           text="Замовити"
           type="outline"
